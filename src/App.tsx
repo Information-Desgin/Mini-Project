aa0c9cd79add07acfb3cbf3d435d9ea4a98402ed
DaheeJang <jecydahee77@gmail.com>
2025-11-02 23:14:03 +0900

    style: 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <div className="w-screen h-screen overflow-hidden flex flex-col px-60 py-12">
-        {/* 제목 섹션 */}
-        <div className="title flex flex-col gap-4">
+      <div className="w-screen h-screen overflow-hidden flex flex-col px-60 py-13">
+        {/* 제목 */}
+        <section className="title flex flex-col gap-3">
           <h1>
             Analyzing the Relationship Between
             <br />
@@ -13,9 +13,9 @@ function App() {
             An intuitive view to compare on-chain activity and market reactions
             in real time
           </h2>
-        </div>
+        </section>
         {/* 범례 */}
-        <div className="label pt-6 flex gap-7 pb-6">
+        <section className="label pt-10 flex gap-7 pb-5">
           <div className="flex gap-2.5 items-center">
             <div className="w-[15px] h-[15px] rounded-full bg-main1" />
             <div>ATOM Price</div>
@@ -28,11 +28,11 @@ function App() {
             <div className="w-[15px] h-[15px] rounded-full bg-main3" />
             <div>Value</div>
           </div>
-        </div>
+        </section>
         {/* 그래프 */}
-        <div className="flex-1 bg-gray-800 rounded-2xl flex justify-center items-center ">
+        <section className="flex-1 bg-gray-800 rounded-2xl flex justify-center items-center ">
           그래프
-        </div>
+        </section>
       </div>
     </>
   );
