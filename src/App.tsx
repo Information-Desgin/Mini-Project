654d8081937b7538ff879de4c3947bd80038179f
DaheeJang <jecydahee77@gmail.com>
2025-11-02 22:53:41 +0900

    style: 스타일 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <div className="title pt-20 pl-65 flex flex-col gap-4">
+        <div className="title pt-12 pl-65 flex flex-col gap-4">
           <h1>
             Analyzing the Relationship Between
             <br />
@@ -12,21 +12,21 @@ function App() {
             An intuitive view to compare on-chain activity and market reactions
             in real time
           </h2>
-          <div className="label pt-13 flex gap-7">
+          <div className="label pt-6 flex gap-7">
             <div className="flex gap-2.5 items-center">
-              <div className="w-4 h-4 rounded-full bg-main1" />
+              <div className="w-[15px] h-[15px] rounded-full bg-main1" />
               <div>ATOM Price</div>
             </div>
             <div className="flex gap-2.5 items-center">
-              <div className="w-4 h-4 rounded-full bg-main2" />
+              <div className="w-[15px] h-[15px] rounded-full bg-main2" />
               <div>Active Account</div>
             </div>
             <div className="flex gap-2.5 items-center">
-              <div className="w-4 h-4 rounded-full bg-main3" />
+              <div className="w-[15px] h-[15px] rounded-full bg-main3" />
               <div>Value</div>
             </div>
           </div>
-          <div className="w-[1018px] h-[533px] bg-gray-800 mt-11 mb-25 rounded-2xl flex justify-center items-center">
+          <div className="w-[1000px] h-[450px] bg-gray-800 mb-25 rounded-2xl flex justify-center items-center">
             그래프
           </div>
         </div>
