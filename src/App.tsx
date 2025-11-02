d1c2c7ea87ef195b3fff71be21419700f3a903a7
DaheeJang <jecydahee77@gmail.com>
2025-11-02 23:04:14 +0900

    style: 그래프에 flex-1 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
   return (
     <>
-      <div>
-        <div className="title pt-12 pl-65 flex flex-col gap-4">
+      <div className="w-screen h-screen overflow-hidden flex flex-col px-60 py-12">
+        {/* 제목 섹션 */}
+        <div className="title flex flex-col gap-4">
           <h1>
             Analyzing the Relationship Between
             <br />
@@ -12,23 +13,25 @@ function App() {
             An intuitive view to compare on-chain activity and market reactions
             in real time
           </h2>
-          <div className="label pt-6 flex gap-7">
-            <div className="flex gap-2.5 items-center">
-              <div className="w-[15px] h-[15px] rounded-full bg-main1" />
-              <div>ATOM Price</div>
-            </div>
-            <div className="flex gap-2.5 items-center">
-              <div className="w-[15px] h-[15px] rounded-full bg-main2" />
-              <div>Active Account</div>
-            </div>
-            <div className="flex gap-2.5 items-center">
-              <div className="w-[15px] h-[15px] rounded-full bg-main3" />
-              <div>Value</div>
-            </div>
+        </div>
+        {/* 범례 */}
+        <div className="label pt-6 flex gap-7 pb-6">
+          <div className="flex gap-2.5 items-center">
+            <div className="w-[15px] h-[15px] rounded-full bg-main1" />
+            <div>ATOM Price</div>
           </div>
-          <div className="w-[1000px] h-[450px] bg-gray-800 mb-25 rounded-2xl flex justify-center items-center">
-            그래프
+          <div className="flex gap-2.5 items-center">
+            <div className="w-[15px] h-[15px] rounded-full bg-main2" />
+            <div>Active Account</div>
           </div>
+          <div className="flex gap-2.5 items-center">
+            <div className="w-[15px] h-[15px] rounded-full bg-main3" />
+            <div>Value</div>
+          </div>
+        </div>
+        {/* 그래프 */}
+        <div className="flex-1 bg-gray-800 rounded-2xl flex justify-center items-center ">
+          그래프
         </div>
       </div>
     </>
